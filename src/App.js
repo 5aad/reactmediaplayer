2a431d0dd954590332bda42647b04ac69af80e0c
Saad <saad_anwer@live.com>
2020-11-21 08:29:11 +0500

    audio

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ import ReactWebMediaPlayer from "react-web-media-player";
 function App() {
   return (
     <div className="App">
-      
       <ReactWebMediaPlayer
-        title="Big Buck Bunny - 2008"
-        thumbnail="https://i.ytimg.com/vi/aqz-KE-bpKQ/maxresdefault.jpg"
-        video="https://nusid.net/video.mp4"
+        title="Key Notez - Summer"
+        thumbnail={"https://nusid.net/album-cover.jpg"}
+        audio="https://nusid.net/audio.mp3"
+        vinyl={{ img: "https://nusid.net/album-cover.jpg", rpm: 10 }}
         logo={{
           img: "https://nusid.net/your-logo.png",
           href: "https://www.npmjs.com/package/react-web-media-player",
         }}
-        width={800}
-        height={450}
-        style={{ margin:'auto' }}
+        width={400}
+        height={400}
+        style={{ margin: "auto" }}
       />
-      </div>
- 
+    </div>
   );
 }
 
